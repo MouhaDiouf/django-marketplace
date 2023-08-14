2e1cbb392280e3ebfc7cbbfdb263dab6e25eec53
MouhaDiouf <mouhamadoudioufsn@gmail.com>
2023-08-14 19:25:58 +0000

    signup

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import index, contact
+from .views import index, contact, signup
 
+
+app_name = "core"
 
 urlpatterns = [
     path('', index, name="index"),
-    path('contact/', contact, name="contact")
+    path('contact/', contact, name="contact"),
+    path('signup', signup, name="signup")
 ]
--- item/views.py
+++ item/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Item
+
 # Create your views here.
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -8,3 +9,4 @@ def detail(request, pk):
         "item": item,
         "related_items": related_items
     })
+
